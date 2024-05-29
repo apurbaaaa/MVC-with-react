27732a975767925bc30fa99ef56d4f2906691229
apurbaaaa <apurbakoirala33@gmail.com>
2024-05-29 11:33:13 +0545

    chore: components defined

--- day2/src/view/SearchBar.jsx
+++ day2/src/view/SearchBar.jsx
@@ -1,16 +1,17 @@
 import React from "react";
-import { useState } from "react";
+
 
 function SearchBar({ inputText, handleInputChange }){
-    
 
     return(
         <div>
-            <label>Location</label>
+            <label>
+                Location 
+
+            </label>
             <input type="text" value={inputText} onChange={handleInputChange}/>
         </div>
-
-    )
+    );
 }
 
 export default SearchBar;

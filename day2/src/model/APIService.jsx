e10f018530522e96517845920c747ff9648e4061
apurbaaaa <apurbakoirala33@gmail.com>
2024-05-31 14:30:37 +0545

    feat: environment variables used

--- day2/src/model/APIService.jsx
+++ day2/src/model/APIService.jsx
@@ -7,15 +7,15 @@ function useFetchData(location) {
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        const fetchData = async () => {//async and await used because axios getting takes a while 
-            const api = '58582bab7d8c113dedcb72a2ba71a6ca';
+        const fetchData = async () => {
+            const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
             try {
-                const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${api}`);
+                const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${apiKey}`);
                 if (response.data) {
                     console.log(response.data);
                     const { name, main, dt } = response.data;
                     const temperature = main.temp;
-                    const time = new Date(dt * 1000).toLocaleTimeString();//*1000 from seconfs to ms 
+                    const time = new Date(dt * 1000).toLocaleTimeString(); // Convert from seconds to milliseconds
                     setData({ place: name, temperature, time });
                 } else {
                     setError('No data received from the server.');
@@ -32,7 +32,7 @@ function useFetchData(location) {
         }
     }, [location]);
 
-    return { data, loading, error };//loading and error sent from the Model but not used by the controller
+    return { data, loading, error };
 }
 
 export default useFetchData;
